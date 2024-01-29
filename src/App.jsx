13e41094dfc87989dc3d46a1e95d917055e63d5b
Tomaz <tomhoq2003@gmail.com>
2024-01-29 14:26:59 +0000

    animations between pages working, lacking polish on arrows

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { AnimatePresence, motion } from "framer-motion"
 import left from './assets/left_arrow.png'
 import right from './assets/right_arrow.png'
 import fig from './assets/fig.png'
@@ -16,16 +17,36 @@ import './App.css'
 
 function App() {
   const [page, setpage] = useState(0);
+  const [direction, setdirection] = useState(0);
 
-  const setPersonal = () => {
+  const PERSONAL = 1;
+  const RESUME = 2;
+  const CONTACTS = 3;
+
+  const setPersonal = (prevpage) => {
+    if(prevpage > PERSONAL){
+      setdirection(-1);
+    } else {
+      setdirection(1);
+    }
     setpage(1);
   }
 
-  const setResume = () => {
+  const setResume = (prevpage) => {
+    if(prevpage > RESUME){
+      setdirection(-1);
+    } else {
+      setdirection(1);
+    }
     setpage(2);
   }
 
-  const setContacts = () => {
+  const setContacts = (prevpage) => {
+    if(prevpage > CONTACTS){
+      setdirection(-1);
+    } else {
+      setdirection(1);
+    }
     setpage(3);
   }
 
@@ -40,6 +61,7 @@ function App() {
   };
 
   const nextPage = () => {
+    setdirection(1);
     if(page === 1){
       setpage(2);
     } else if(page === 2){
@@ -48,6 +70,7 @@ function App() {
   }
 
   const previousPage = () => {
+    setdirection(-1);
     if(page === 2){
       setpage(1);
     } else if(page === 3){
@@ -55,9 +78,27 @@ function App() {
     }
   }
 
+  const variants = {
+    initial: direction => { 
+      return {
+        opacity: 0,
+        marginRight: direction === 1 ? -200 : 200,
+      }
+    },
+    animate: {
+      opacity: 1,
+      marginRight: 0,
+    },
+    exit: direction => { 
+      return {
+        opacity: 0,
+        marginRight: direction === 1 ? -200 : 200,
+      }
+    },
+  }
+
   return (
     <div id="root">
-      {/* Move the link elements to the head of your HTML document */}
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -66,9 +107,9 @@ function App() {
 
       <div className="card">
         <div className="navbar">
-          <h2 onClick={setPersonal} className={page===1 ? "clicked":""} >Personal</h2>
-          <h2 onClick={setResume} className={page===2 ? "clicked":""} >Resume</h2>
-          <h2 onClick={setContacts} className={page===3 ? "clicked":""} >Contact me</h2>
+          <h2 onClick={() => setPersonal(page)} className={page===1 ? "clicked":""} >Personal</h2>
+          <h2 onClick={() => setResume(page)} className={page===2 ? "clicked":""} >Resume</h2>
+          <h2 onClick={() => setContacts(page)} className={page===3 ? "clicked":""} >Contact me</h2>
         </div>
 
         <div className="body">
@@ -87,70 +128,93 @@ function App() {
           </div>
           <div className="right">
             { page !== 0 && page !== 1? <img className="arrow" onClick={previousPage} src={left}></img> : <></>}
+            <div className='page'
+            >
+              { page === 0 && <> </>}
+              { page === 2 &&
+                  <motion.div variants={variants} 
+                    animate="animate" 
+                    initial="initial" 
+                    exit="exit"
+                    className="personal"
+                    custom={direction}>
+                    <img className="cv" src={cv}></img> 
+                    <button onClick={download} className='down'>Download</button>
+                  </motion.div>}
 
-            { page === 0 ? <> </>:
-              page === 2 ?  
-                <div className="personal">
-                  <img className="cv" src={cv}></img> 
-                  <button onClick={download} className='down'>Download</button>
-                </div> : 
-              page === 1 ? 
-                <div className="about">
-                  <h2>More about me</h2>
-                  <div className='subheader'>
-                    <h3>Academically focused</h3>
-                    <img classname="book" src={book} alt="" />
-                  </div>
-
-                  <p>Finished secondary with an average of 18.9/20 
-                    and will soon end my bachelor with a predicted 15.7.</p>
-                  <div className='subheader'>
-                    <h3>Machine learning Enthusiast</h3>
-                    <img classname="book" src={ml} alt="" />
-                  </div>
-                  <p>Finished the Machine Learning subject with a 19/20.
-                    I often read articles on the area about latest breakthroughs.
-                    I have also taken some online courses like Google’s “Introduction to Generative AI”
-                    and participate in challenges provided by Kaggle to increase my knowledge of NNs.
-                  </p>
-                  <div className='subheader'>
-                    <h3>Versatile Athlete</h3>
-                    <img classname="book" src={bolt} alt="" />
-                  </div>
-                  <p>I started playing roller hockey and tennis at 5, 
-                    competing in national championships in both and earning awards. 
-                    Additionally, I'm a regular gym-goer and run two half marathons each year. 
-                    Exercise is a big part of my life and I always make time for it.
-                  </p>
-                  <div className='subheader'>
-                    <h3>Curiosity driven</h3>
-                    <img className="lupa" src={lupa} alt="" />
-                  </div>
-                  <p>Always learning something new, whether it's a new instrument 
-                    (<a href='https://www.youtube.com/watch?v=m0SpYPigoaM'>guitar</a>), a new language (Italian), or a 
-                    framework/programming language (React and Solidity, 2023).
-                  </p>
-                </div>: 
-              page === 3 ? 
-              <div className="contacts">
-                <h2>Location</h2>
-                <p>Loures, Portugal</p>
-                <h2>Email</h2>
-                <p>tomaz.goncalves-silva@tecnico.ulisboa.pt</p>
-                <h2>Internship/Part-time</h2>
-                <p>Available</p>
-                <div className="media">
-                  <div className="same">
-                    <img src={linkedin} className='empty' onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
-                    <img src={l} className="full" onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
-                  </div>
-                  <div className="same">
-                    <img src={github} className='empty' onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
-                    <img src={g} className="full" onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
-                  </div>
-                </div> 
-                
-              </div> : <></>}
+              { page === 1 &&
+                  <motion.div
+                  variants={variants}
+                  animate="animate"
+                  initial="initial"
+                  exit="exit"
+                  className="about"
+                  custom={direction}
+                >
+                    <h2>More about me</h2>
+                    <div className='subheader'>
+                      <h3>Academically focused</h3>
+                      <img className="book" src={book} alt="" />
+                    </div>
+                    <p>Finished secondary with an average of 18.9/20 
+                      and will soon end my bachelor with a predicted 15.7.</p>
+                    <div className='subheader'>
+                      <h3>Machine learning Enthusiast</h3>
+                      <img className="book" src={ml} alt="" />
+                    </div>
+                    <p>Finished the Machine Learning subject with a 19/20.
+                      I often read articles on the area about latest breakthroughs.
+                      I have also taken some online courses like Google’s “Introduction to Generative AI”
+                      and participate in challenges provided by Kaggle to increase my knowledge of NNs.
+                    </p>
+                    <div className='subheader'>
+                      <h3>Versatile Athlete</h3>
+                      <img className="book" src={bolt} alt="" />
+                    </div>
+                    <p>I started playing roller hockey and tennis at 5, 
+                      competing in national championships in both and earning awards. 
+                      Additionally, I'm a regular gym-goer and run two half marathons each year. 
+                      Exercise is a big part of my life and I always make time for it.
+                    </p>
+                    <div className='subheader'>
+                      <h3>Curiosity driven</h3>
+                      <img className="lupa" src={lupa} alt="" />
+                    </div>
+                    <p>Always learning something new, whether it's a new instrument 
+                      (<a href='https://www.youtube.com/watch?v=m0SpYPigoaM'>guitar</a>), a new language (Italian), or a 
+                      framework/programming language (React and Solidity, 2023).
+                    </p>
+                  </motion.div>
+              }
+              { page === 3 &&
+                <motion.div
+                variants={variants}
+                animate="animate"
+                initial="initial"
+                exit="exit"
+                className="contacts"
+                custom={direction}
+                >
+                  <h1>Let's get in touch</h1>
+                  <h2>Location</h2>
+                  <p>Loures, Portugal</p>
+                  <h2>Email</h2>
+                  <p>tomaz.goncalves-silva@tecnico.ulisboa.pt</p>
+                  <h2>Internship/Part-time</h2>
+                  <p>Available</p>
+                  <div className="media">
+                    <div className="same">
+                      <img src={linkedin} className='empty' onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
+                      <img src={l} className="full" onClick={() => window.open("https://www.linkedin.com/in/tomaz-silva-555397227/")} alt="" />
+                    </div>
+                    <div className="same">
+                      <img src={github} className='empty' onClick={() => window.open("https://github.com/tomhoq")} alt="" />
+                      <img src={g} className="full" onClick={() => window.open("https://github.com/tomhoq")} alt="" />
+                    </div>
+                  </div> 
+                </motion.div>
+              }
+            </div>
             { page !== 0 && page !== 3? <img className="arrow" onClick={nextPage} src={right}></img> : <></>}
           </div>
         </div>
